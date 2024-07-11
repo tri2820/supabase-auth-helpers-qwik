1abf9919ab5dfeed61592137536b2071993eb6aa
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2024-07-11 19:44:42 +0700

    feat:

---
+++ lib/types/index.d.ts
@@ -0,0 +1,3 @@
+export * from '../index'
+import _default from '../index'
+export default _default
--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
   plugins: [
     dts({
       include: ["src"],
+      insertTypesEntry: true,
     }),
   ],
 });
